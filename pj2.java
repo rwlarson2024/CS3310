5bb4b0b5658f1a311a5f237d648e13de183d735f
Ryan <98720555+rwlarson2024@users.noreply.github.com>
2023-03-09 19:22:16 -0800

    Add files via upload

---
+++ pj2.java
@@ -0,0 +1,29 @@
+package Project2;
+import java.util.*;
+public class pj2 
+{
+    public static void main (String[] args)
+    {
+        
+    }
+    public static double[] algorithm1(double k, double[] S)
+    {
+        double temp[] = new double[S.length];
+        double m = (S.length-1)/2;
+        if(S.length == 1)
+        {
+            return S[0];
+        }
+        else
+        {
+            double s1 = 0;
+            double s2 = m;
+            double s3 = m/2;
+
+        }
+    }
+    public static algorithm2()
+    {
+
+    }
+}
