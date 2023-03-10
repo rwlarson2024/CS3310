936962de3de6b54a6450330b08d23e7023cdc1bf
Ryan <98720555+rwlarson2024@users.noreply.github.com>
2023-03-09 21:57:10 -0800

    Add files via upload

--- pj2.java
+++ pj2.java
@@ -1,4 +1,3 @@
-package Project2;
 import java.util.*;
 public class pj2 
 {
@@ -6,24 +5,91 @@ public class pj2
     {
         
     }
-    public static double[] algorithm1(double k, double[] S)
+    public static int partition(int[] S, int low, int high)
     {
-        double temp[] = new double[S.length];
-        double m = (S.length-1)/2;
-        if(S.length == 1)
+        int a = S[high];
+        int i = low;
+        for(int j = low; j <= high -1; j++)
         {
-            return S[0];
+            if(S[j] <= a)
+            {
+                int temp = S[i];
+                S[i] = S[j];
+                S[j] = temp;
+                i++;
+            }
         }
-        else
-        {
-            double s1 = 0;
-            double s2 = m;
-            double s3 = m/2;
-
-        }
+        int temp = S[i];
+        S[i] = S[high];
+        S[high] = temp;
+        return i; 
     }
-    public static algorithm2()
+    public static int kthSmalllest(int S[], int low, int high, int k)
     {
+        if(k > 0 && k <= high - low + 1)
+        {
+            int pos = partition(S,low,high);
+            if(pos - low == k-1)
+                return S[pos];
+            if(pos-low > k -1 )
+                return kthSmalllest(S, pos+1, high, k - pos + low -1);
+        }
+        return Integer.MAX_VALUE;
 
+    }
+    void merge (int[] S,int low, int mid, int high)
+    {
+        int n1 = mid - low + 1;
+        int n2 = high - mid; 
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for(int i = 0 ; i < n1 ; i++)
+            L[i] = S[mid + i];
+
+        for(int j = 0 ; j < n2 ; j++)
+            R[j] = S[mid + 1 +j];
+        int i = 0;
+        int j = 0;
+        int k = 1;
+
+        while(i < n1 && j < n2)
+        {
+            if(L[i] <= R[j])
+            {
+                S[k] = L[i];
+                i++;
+            }
+            else
+            {
+                S[k] = L[i];
+                j++;
+            }
+            k++;
+        } 
+        //copy remaining elements into S[] from L[]
+        while (i < n1)
+        {
+            S[k] = L[i];
+            i++;
+            k++;
+        }
+        //copy remaining elements into S[] from R[]
+        while(j < n2)
+        {
+            S[k] = R[j];
+            j++;
+            k++;
+        } 
+    }
+    void sort(int[] S, int low, int high)
+    {
+        if (low < high)
+        {
+            int m  = low + (high - 1)/2;
+            sort(S,low,m);
+            sort(S,m+1,high);
+            merge(S, low, m, high);
+        }
     }
 }
