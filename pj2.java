b861c33165a9833b91ed5372e8610eda39e89e1c
Ryan <98720555+rwlarson2024@users.noreply.github.com>
2023-03-12 21:39:04 -0700

    Add files via upload

--- pj2.java
+++ pj2.java
@@ -9,40 +9,72 @@ public class pj2
         System.out.println("Enter a value for the kth smallest value");
         int k = keyboard.nextInt();
         int[] array = RandomNumberGenerator(n);
+        
         int low = 0;
         int high = array.length;
-        System.out.println("K'th smallest element is: " + kthSmalllestfifty(array, low, high, k));
+
+        System.out.println("K'th smallest element for quickselect's processing is: " );
+        long Start = System.nanoTime();
+        int sol1 = kthSmalllest(array, k);
+        long End = System.nanoTime();
+        long CPUTime = End - Start;
+        System.out.println(sol1);
+        System.out.println("The CPU time of quickselect is : " + CPUTime );
+        printarray(array);
+        System.out.println();
+
+        System.out.println("K'th smallest element for four-fifth's processing is: " );
+        long Start2 = System.nanoTime();
+        int sol2 = kthSmalllestfifty(array, low, high-1, k);
+        long End2 = System.nanoTime();
+        long CPUTime2 = End2 - Start2;
+        System.out.println(sol2);
+        System.out.println("The CPU time of four-fifths is : " + CPUTime2 );
+        printarray(array);
+        System.out.println();
+
 
         
+    }
+
+    public static void printarray (int[] s)
+    {
+        int i;
+        for(i=0; i < s.length;i++)
+        {
+            System.out.print(s[i]+" ");
+        }
     }
     public static int [] RandomNumberGenerator (int n)
     {
         int[] array = new int[n];
-        Random random = new Random();
-        for(int i = 0; i < array.length; i++)
+        Random rand = new Random();
+        for(int i = 0; i < n; i++)
         {
-            array[i] = random.nextInt(100);
+            array[i] = rand.nextInt(100);
         }
         return array;
+       
     }
     public static int partition(int[] S, int low, int high)
     {
-        int a = S[high];
+        int pivot = S[high];
         int i = low;
-        for(int j = low; j <= high -1; j++)
+        for(int j = low; j < high; j++)
         {
-            if(S[j] <= a)
+            if(S[j] < pivot)
             {
-                int temp = S[i];
-                S[i] = S[j];
-                S[j] = temp;
+                swap2(S, i, j);
                 i++;
             }
         }
-        int temp = S[i];
-        S[i] = S[high];
-        S[high] = temp;
-        return i; 
+        swap2(S, i, high);
+        return i;
+
+    }
+    public static int kthSmalllest(int[]s, int k)
+    {
+        return kthSmalllest(s, 0, s.length-1, k-1); 
     }
     //searches array for value x and patitions array around the value x
     public static int partition2(int S[], int low, int high, int x)
@@ -69,15 +101,23 @@ public class pj2
     }
     public static int kthSmalllest(int S[], int low, int high, int k)
     {
-        if(k > 0 && k <= high - low + 1)
+        if(low == high)
         {
-            int pos = partition(S,low,high);
-            if(pos - low == k-1)
-                return S[pos];
-            if(pos-low > k -1 )
-                return kthSmalllest(S, pos+1, high, k - pos + low -1);
+            return S[low];
         }
-        return Integer.MAX_VALUE;
+        int pivotPoint = partition(S, low, high);
+        if(k == pivotPoint)
+        {
+            return S[k];
+        }
+        else if (k< pivotPoint)
+        {
+            return kthSmalllest(S, low, pivotPoint - 1, k);
+        }
+        else 
+        {
+            return kthSmalllest(S, pivotPoint + 1, high, k);
+        }
 
     }
     static void merge (int[] S,int low, int mid, int high)
@@ -135,6 +175,12 @@ public class pj2
             merge(S, low, m, high);
         }
     }
+    static void swap2(int[] s, int i, int j)
+    {
+        int temp = s[i];
+        s[i]= s[j];
+        s[j] = temp;
+    }
     static int[] swap(int[] S, int i, int j)
     {
         int temp = S[i];
@@ -145,25 +191,25 @@ public class pj2
     static int findMedian(int S[], int i, int j)
     {
         Arrays.sort(S, i , j);
-        return S[i+(j-1)/2];
+        return S[i+(j-i)/2];
     }
     public static int kthSmalllestfifty(int S[], int low, int high, int k)
     {
         int n = S.length;
-        if(n < 50)
+        /*if(n < 50)
         {
             sort(S, low, high);
             return S[0];
 
-        }
+        }*/
         if(k > 0 && k <= high - low + 1)
         {
-            n = high - low +1;
+            n = high - low + 1;
             int i; 
             int median[] = new int[(n+4)/5];
             for(i = 0; i < n/5; i++)
             {
-                median[i] = findMedian(S, low + i*5, low + i *5+5);
+                median[i] = findMedian(S, low+i*5, low+i*5+5);
             }
             if(i*5 < n)
             {
@@ -177,12 +223,12 @@ public class pj2
                                     kthSmalllestfifty(median, 0,i-1,i/2);
             int pos = partition2(S, low, high, medianofMedian);
 
-            if(pos -low == k-1)
+            if(pos - low == k - 1)
                 return S[pos];
             //recur for left array
-            if(pos - low > k -1 )
+            if(pos - low > k - 1)
                 return kthSmalllestfifty(S, low, pos-1, k);
-            return kthSmalllestfifty(S, pos +1, high, k - pos +low -1);
+            return kthSmalllestfifty(S, pos + 1, high, k - pos + low - 1);
         }
         return Integer.MAX_VALUE;
     }
